7bb6837289d9cea9ec6ca0c06500ebe62b0ccf67
Flor Marshall <flordmarshall@gmail.com>
2022-06-28 14:52:48 -0400

    css and title border

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ function App() {
       <header className="App-header">
         <Title />
       </header>
-
       <section></section>
     </div>
   );
--- src/components/title.tsx
+++ src/components/title.tsx
@@ -3,6 +3,10 @@ import pizzaTitle from "../the_pizza_title.png";
 export default function Title() {
   return (
     <div>
+      <span className="Text-wrapper">
+        <p className="Title-text">The History of</p>
+      </span>
+
       <img src={pizzaTitle} alt="The Pizza" />
     </div>
   );
