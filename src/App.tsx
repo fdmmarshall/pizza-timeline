7b6c2aea22af675818336780e0f4ea10ab4eb020
Flor Marshall <flordmarshall@gmail.com>
2022-06-30 11:13:57 -0400

    change section to footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "components/title";
 import PizzaSection from "components/pizza";
 import "./App.css";
 
 function App() {
+  const [wholePizzaDisplay, setWholePizzaDisplay] = useState<boolean>(true);
+  const [splitPizzaDisplay, setSplitPizzaDisplay] = useState<boolean>(false);
+
   return (
     <div className="App">
       <header className="App-header">
         <Title />
       </header>
-      <section>
-        <PizzaSection />
-      </section>
+      <footer className="App-footer">
+        <PizzaSection display={wholePizzaDisplay} />
+      </footer>
     </div>
   );
 }
