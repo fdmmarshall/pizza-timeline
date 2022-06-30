6db0be525f4caef0cb45a4367fd0da6257656a40
Flor Marshall <flordmarshall@gmail.com>
2022-06-30 14:48:50 -0400

    add timeline css and component

---
+++ src/components/PizzaTimeline.tsx
@@ -0,0 +1,9 @@
+// import { classNames } from "../helpers/helperFunctions";
+
+interface PizzaTimelineProps {
+  display: boolean;
+}
+
+export default function TimelineContainer({ display }: PizzaTimelineProps) {
+  return <></>;
+}
