b0c47d8e26d62b93766a9c5d666db652a370a4cb
Flor Marshall <flordmarshall@gmail.com>
2022-06-30 11:38:49 -0400

    new create new component

---
+++ src/components/SplitPizza.tsx
@@ -0,0 +1,25 @@
+import { classNames } from "../helpers/helperFunctions";
+import topPizzaHalf from "../top_pizza_half.png";
+import bottomPizzaHalf from "../bottom_pizza_half.png";
+
+interface PizzaProps {
+  display: boolean;
+}
+
+export default function PizzaSection({ display }: PizzaProps) {
+  return (
+    <div className="Pizza-section" id="Pizza-section">
+      <img
+        src={topPizzaHalf}
+        alt="Top pizza half"
+        className={classNames(display === true ? "display" : "hide", "Pizza")}
+      />
+      <div id="White-space"></div>
+      <img
+        src={bottomPizzaHalf}
+        alt="Bottom pizza half"
+        className={classNames(display === true ? "display" : "hide", "Pizza")}
+      />
+    </div>
+  );
+}
