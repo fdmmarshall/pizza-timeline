e3d9abd1eb3bcf2801581dc9b9b665df8e4a9e32
Flor Marshall <flordmarshall@gmail.com>
2022-06-30 11:14:51 -0400

    add class to div

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -2,7 +2,7 @@ import pizzaTitle from "../the_pizza_title.png";
 
 export default function Title() {
   return (
-    <div>
+    <div className="Title-section">
       <span className="Text-wrapper">
         <p className="Title-text">The History of</p>
       </span>
