12a3bc7d0f942da4ca791a76e3d746243c1d1a75
Flor Marshall <flordmarshall@gmail.com>
2022-07-01 14:54:44 -0400

    remove unnecessary code

--- src/components/TimelineContainer.tsx
+++ src/components/TimelineContainer.tsx
@@ -1,21 +1,10 @@
-import { classNames } from "../helpers/helperFunctions";
 import PizzaTimeline from "./PizzaTimeline";
 
-interface ContainerProps {
-  display: boolean;
-}
-
-export default function TimelineContainer({ display }: ContainerProps) {
+export default function TimelineContainer() {
   return (
-    <div
-      className={classNames(
-        display === true ? "display" : "hide",
-        "Timeline-container"
-      )}
-      id="white-space"
-    >
+    <div className="Timeline-container" id="white-space">
       <h1>THE ORIGIN STORY OF PIZZA</h1>
-      <PizzaTimeline display={display} />
+      <PizzaTimeline />
     </div>
   );
 }
