790057b14bd2c5bfcbc4345189ec23a79d61ecbc
Flor Marshall <flordmarshall@gmail.com>
2022-07-02 22:39:05 -0400

    start timeline and css

--- src/components/PizzaTimeline.tsx
+++ src/components/PizzaTimeline.tsx
@@ -2,81 +2,26 @@ import "../style_assets/timeline.css";
 
 export default function TimelineContainer() {
   return (
-    <>
-      <ul className="timeline" id="timeline">
-        <li className="dot">
-          <div className="timestamp-top">
-            <span className="icon"></span>
-            <span className="pizza-info">
-              <h3>79 A.D.</h3>
-              <p></p>
-            </span>
-          </div>
-        </li>
-        <li className="dot">
-          <div className="timestamp-bottom">
-            <span className="icon"></span>
-            <span className="pizza-info">
-              <h3>1522</h3>
-              <p></p>
-            </span>
-          </div>
-        </li>
-        <li className="dot">
-          <div className="timestamp-top">
-            <span className="icon"></span>
-            <span className="pizza-info">
-              <h3>1880</h3>
-              <p></p>
-            </span>
-          </div>
-        </li>
-        <li className="dot">
-          <div className="timestamp-bottom">
-            <span className="icon"></span>
-            <span className="pizza-info">
-              <h3>1889</h3>
-              <p></p>
-            </span>
-          </div>
-        </li>
-        <li className="dot">
-          <div className="timestamp-top">
-            <span className="icon"></span>
-            <span className="pizza-info">
-              <h3>1905</h3>
-              <p></p>
-            </span>
-          </div>
-        </li>
-        <li className="dot">
-          <div className="timestamp-bottom">
-            <span className="icon"></span>
-            <span className="pizza-info">
-              <h3>1943</h3>
-              <p></p>
-            </span>
-          </div>
-        </li>
-        <li className="dot">
-          <div className="timestamp-top">
-            <span className="icon"></span>
-            <span className="pizza-info">
-              <h3>1958</h3>
-              <p></p>
-            </span>
-          </div>
-        </li>
-        <li className="dot">
-          <div className="timestamp-bottom">
-            <span className="icon"></span>
-            <span className="pizza-info">
-              <h3>TODAY</h3>
-              <p></p>
-            </span>
-          </div>
-        </li>
-      </ul>
-    </>
+    <div className="wrapper">
+      <section className="horizontal-timeline-container">
+        <div className="timeline-block">
+          <span>hello</span>
+        </div>
+        <div className="line"></div>
+        <div className="timeline-block"></div>
+        <div className="line"></div>
+        <div className="timeline-block"></div>
+        <div className="line"></div>
+        <div className="timeline-block"></div>
+        <div className="line"></div>
+        <div className="timeline-block"></div>
+        <div className="line"></div>
+        <div className="timeline-block"></div>
+        <div className="line"></div>
+        <div className="timeline-block"></div>
+        <div className="line"></div>
+        <div className="timeline-block"></div>
+      </section>
+    </div>
   );
 }
