8fbdf869d57599cc3ef82744ae079a763acfcf1a
Flor Marshall <flordmarshall@gmail.com>
2022-07-06 20:21:16 +0200

    layout + css adjustment

--- src/components/PizzaTimeline.tsx
+++ src/components/PizzaTimeline.tsx
@@ -3,25 +3,123 @@ import "../style_assets/timeline.css";
 export default function TimelineContainer() {
   return (
     <div className="wrapper">
-      <section className="horizontal-timeline-container">
-        <div className="timeline-block">
-          <span>hello</span>
+      <div className="top-content">
+        <div className="content-container item-one">
+          <div className="content">
+            <div className="svg">SVG</div>
+            <div className="info">
+              <h3>79 A.D.</h3>
+              <p>text goes here</p>
+            </div>
+          </div>
         </div>
-        <div className="line"></div>
-        <div className="timeline-block"></div>
-        <div className="line"></div>
-        <div className="timeline-block"></div>
-        <div className="line"></div>
-        <div className="timeline-block"></div>
-        <div className="line"></div>
-        <div className="timeline-block"></div>
-        <div className="line"></div>
-        <div className="timeline-block"></div>
-        <div className="line"></div>
-        <div className="timeline-block"></div>
-        <div className="line"></div>
-        <div className="timeline-block"></div>
-      </section>
+        <div className="content-container"></div>
+        <div className="content-container item-three">
+          <div className="content">
+            <div className="svg">SVG</div>
+            <div className="info">
+              <h3>1880</h3>
+              <p>text goes here</p>
+            </div>
+          </div>
+        </div>
+        <div className="content-container"></div>
+        <div className="content-container item-five">
+          <div className="content">
+            <div className="svg">SVG</div>
+            <div className="info">
+              <h3>1905</h3>
+              <p>text goes here</p>
+            </div>
+          </div>
+        </div>
+        <div className="content-container"></div>
+        <div className="content-container item-seven">
+          <div className="content">
+            <div className="svg">SVG</div>
+            <div className="info">
+              <h3>1958</h3>
+              <p>text goes here</p>
+            </div>
+          </div>
+        </div>
+        <div className="content-container"></div>
+      </div>
+      <div className="horizontal-timeline">
+        <div className="container">
+          <div className="dot"></div>
+          <hr />
+        </div>
+        <div className="container">
+          <div className="dot"></div>
+          <hr />
+        </div>
+        <div className="container">
+          <div className="dot"></div>
+          <hr />
+        </div>
+        <div className="container">
+          <div className="dot"></div>
+          <hr />
+        </div>
+        <div className="container">
+          <div className="dot"></div>
+          <hr />
+        </div>
+        <div className="container">
+          <div className="dot"></div>
+          <hr />
+        </div>
+        <div className="container">
+          <div className="dot"></div>
+          <hr />
+        </div>
+        <div>
+          <div className="dot"></div>
+        </div>
+      </div>
+      <div className="bottom-content">
+        <div className="content-container"></div>
+        <div className="content-container item-two">
+          <div className="content">
+            <div className="svg">SVG</div>
+            <div className="info">
+              <h3>1522</h3>
+              <p>text goes here</p>
+            </div>
+          </div>
+        </div>
+        <div className="content-container"></div>
+        <div className="content-container item-four">
+          <div className="content">
+            <div className="svg">SVG</div>
+            <div className="info">
+              <h3>1889</h3>
+              <p>text goes here</p>
+            </div>
+          </div>
+        </div>
+        <div className="content-container"></div>
+        <div className="content-container item-six">
+          <div className="content">
+            <div className="svg">SVG</div>
+            <div className="info">
+              <h3>1943</h3>
+              <p>text goes here</p>
+            </div>
+          </div>
+        </div>
+        <div className="content-container"></div>
+        <div className="content-container item-eight">
+          <div className="content">
+            <div className="svg">SVG</div>
+            <div className="info">
+              <h3>TODAY</h3>
+              <p>text goes here</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
